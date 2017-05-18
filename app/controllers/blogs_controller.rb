0628a7d2664d5825a4fe3db3c35eb7317ff1d2e6
Jorge Sevilla <jorgesevilla7@gmail.com>
2017-05-17 21:36:12 -0700

    Added nav bar and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,14 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "JSevilla Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @page_title = "JSevDev | #{@blog.title}" 
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
