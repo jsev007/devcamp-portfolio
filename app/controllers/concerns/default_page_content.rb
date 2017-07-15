a5dc4162760037ed80db36e632030ccafd4a1cad
Jorge Sevilla <jorgesevilla7@gmail.com>
2017-07-14 20:04:53 -0700

    Integrated production calls for action cable

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "DevcampPortfolio | My Portfolio Website"
+    @page_title = " JSev | My Portfolio Website"
     @seo_keywords = "Jorge Sevilla Portfolio"
   end
 end
